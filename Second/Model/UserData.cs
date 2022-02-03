933930c6797c4e67c045fc657a185afbc8e28fc9
Michal Stehlík <michal.stehlik@pslib.cz>
2022-02-03 13:32:10 +0100

    razor syntax I

---
+++ Second/Model/UserData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Second.Model
+{
+    public class UserData
+    {
+        public string Firstname { get; set; }
+    }
+}
---
+++ Second/Pages/Privacy.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Second.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Second.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public int Number { get; set; } = 7;
+
+        public string Text { get; set; } = "Ahoj";
+        public bool Fake { get; set; } = true;
+
+        public UserData FavoriteUser{ get; set; } = new UserData { Firstname = "Květoslav"};
+        public List<UserData> List { get; set; } = new List<UserData> {
+            new UserData { Firstname = "Antonín"},
+            new UserData { Firstname = "Bořivoj"},
+            new UserData { Firstname = "Ctibor"},
+            new UserData { Firstname = "Dalimil"},
+            new UserData { Firstname = "Evžen"},
+            new UserData { Firstname = "Flora"},
+            new UserData { Firstname = "Gretchen"}
+        };
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
